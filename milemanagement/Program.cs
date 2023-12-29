e7c83c75cbc0f17c00384ea284c55b388b7815a7
Orlando Adams <ojadams@student.fullsail.edu>
2023-12-28 19:00:55 -0500

    file input and  out put started

---
+++ milemanagement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+
+
+namespace FileManagementSystem
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        string directoryPath = " ";
+        Console.WriteLine("File Directory Path\n");
+
+        //DisplayFilesInDirectory(directoryPath);
+
+
+        Console.WriteLine("\nOptions: ");
+        Console.WriteLine("1. Create file: ");
+        Console.WriteLine("2. Delete file: ");
+        Console.WriteLine("Enter option number (1, 2): ");
+
+
+    }
+  }
+}
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
